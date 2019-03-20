182693d6318bacbe311f69fa85444abb21d9bb23
tortelle <42223675+tortelle@users.noreply.github.com>
2019-03-20 11:56:01 -0300

    Versão 0.1

---
+++ par_impar/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace par_impar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.nome1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbImpar = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.btnJogar_Click = new System.Windows.Forms.Button();
+            this.placarbt = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nome1
+            // 
+            this.nome1.AutoSize = true;
+            this.nome1.Location = new System.Drawing.Point(17, 14);
+            this.nome1.Name = "nome1";
+            this.nome1.Size = new System.Drawing.Size(94, 13);
+            this.nome1.TabIndex = 0;
+            this.nome1.Text = "Nome do Jogador:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(16, 30);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(95, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(125, 30);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(91, 20);
+            this.txtValor.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Valor:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.rdbImpar);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(16, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 61);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Escolha:";
+            // 
+            // rdbImpar
+            // 
+            this.rdbImpar.AutoSize = true;
+            this.rdbImpar.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbImpar.Location = new System.Drawing.Point(114, 22);
+            this.rdbImpar.Name = "rdbImpar";
+            this.rdbImpar.Size = new System.Drawing.Size(81, 27);
+            this.rdbImpar.TabIndex = 1;
+            this.rdbImpar.TabStop = true;
+            this.rdbImpar.Text = "Impar:";
+            this.rdbImpar.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(6, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(60, 27);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Par:";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // btnJogar_Click
+            // 
+            this.btnJogar_Click.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogar_Click.Location = new System.Drawing.Point(16, 141);
+            this.btnJogar_Click.Name = "btnJogar_Click";
+            this.btnJogar_Click.Size = new System.Drawing.Size(75, 23);
+            this.btnJogar_Click.TabIndex = 6;
+            this.btnJogar_Click.Text = "Jogar";
+            this.btnJogar_Click.UseVisualStyleBackColor = true;
+            this.btnJogar_Click.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // placarbt
+            // 
+            this.placarbt.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.placarbt.Location = new System.Drawing.Point(141, 141);
+            this.placarbt.Name = "placarbt";
+            this.placarbt.Size = new System.Drawing.Size(75, 23);
+            this.placarbt.TabIndex = 7;
+            this.placarbt.Text = "Placar";
+            this.placarbt.UseVisualStyleBackColor = true;
+            this.placarbt.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(235, 176);
+            this.Controls.Add(this.placarbt);
+            this.Controls.Add(this.btnJogar_Click);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.nome1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Par/Impar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nome1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton rdbImpar;
+        private System.Windows.Forms.Button btnJogar_Click;
+        private System.Windows.Forms.Button placarbt;
+    }
+}
+
---
+++ par_impar/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace par_impar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numeroJogador = 0, numeroComputador = 0, total;
+            StreamWriter arquivo;
+            System.IO.Directory.CreateDirectory("C:\\Banco De Dados\\parimpar");
+            String caminho = @"C:\\Banco De Dados\\parimpar\\placar.txt";            
+            arquivo = File.AppendText(caminho);
+            string nome = txtNome.Text;
+            Random dado = new Random();
+            numeroComputador = dado.Next(0, 11);
+
+            numeroJogador = Convert.ToInt32(txtValor.Text);
+            total = (numeroComputador + numeroJogador);
+            MessageBox.Show("Seu Número: " + txtValor.Text + "\nNúmero do computador: " + numeroComputador.ToString() + "\nTotal: " + total.ToString());
+
+            if(rdbImpar.Checked == true)
+            {
+                if(total % 2 != 0)
+                {
+                    MessageBox.Show("Voce Escolheu Impar " + nome + ", voce ganhou ");
+                    arquivo.WriteLine("");
+                    arquivo.Write("Nome: "+txtNome.Text );
+                    arquivo.WriteLine("");
+                    arquivo.Write("Valor Escolhido do "+nome+": " + txtValor.Text);
+                    arquivo.WriteLine("");
+                    arquivo.Write("Valor Escolhido do computador: " + numeroComputador);
+                    arquivo.WriteLine("");
+                    arquivo.Write("Total: " + total);
+                    arquivo.WriteLine("");
+                    arquivo.WriteLine("VENCEDOR ");
+                    arquivo.WriteLine("");
+                    arquivo.WriteLine("________________________________________________");
+                    arquivo.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Voce Escolheu Impar " + nome + ", voce perdeu ");
+                    arquivo.WriteLine("");
+                    arquivo.Write("Nome: " + txtNome.Text);
+                    arquivo.WriteLine("");
+                    arquivo.Write("Valor Escolhido do " + nome + ": " + txtValor.Text);
+                    arquivo.WriteLine("");
+                    arquivo.Write("Valor Escolhido do computador: " + numeroComputador);
+                    arquivo.WriteLine("");
+                    arquivo.Write("Total: " + total);
+                    arquivo.WriteLine("");
+                    arquivo.WriteLine("PERDEDOR ");
+                    arquivo.WriteLine("");
+                    arquivo.WriteLine("________________________________________________");
+                    arquivo.Close();
+                }
+            }
+            else
+            {
+                if (total % 2 == 0)
+                {
+                    MessageBox.Show("Voce Escolheu Par " + nome + ", voce ganhou ");
+                    arquivo.WriteLine("");
+                    arquivo.Write("Nome: " + txtNome.Text);
+                    arquivo.WriteLine("");
+                    arquivo.Write("Valor Escolhido do " + nome + ": " + txtValor.Text);
+                    arquivo.WriteLine("");
+                    arquivo.Write("Valor Escolhido do computador: " + numeroComputador);
+                    arquivo.WriteLine("");
+                    arquivo.Write("Total: " + total);
+                    arquivo.WriteLine("");
+                    arquivo.WriteLine("VENCEDOR ");
+                    arquivo.WriteLine("");
+                    arquivo.WriteLine("________________________________________________");
+                    arquivo.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Voce Escolheu Par " + nome + ", voce perdeu ");
+                    arquivo.WriteLine("");
+                    arquivo.Write("Nome: " + txtNome.Text);
+                    arquivo.WriteLine("");
+                    arquivo.Write("Valor Escolhido do " + nome + ": " + txtValor.Text);
+                    arquivo.WriteLine("");
+                    arquivo.Write("Valor Escolhido do computador: " + numeroComputador);
+                    arquivo.WriteLine("");
+                    arquivo.Write("Total: " + total);
+                    arquivo.WriteLine("");
+                    arquivo.WriteLine("PERDEDOR ");
+                    arquivo.WriteLine("");
+                    arquivo.WriteLine("________________________________________________");
+                    arquivo.Close();
+                }
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string caminho = "C:\\Banco De Dados\\parimpar\\placar.txt";
+            System.Diagnostics.Process.Start("NOTEPAD", caminho);
+        }
+        
+    }
+}
